a52918bd697435ce81def1637f6bbc31d1183cda
ObraztsovOleg <olegrasputina@gmail.com>
2022-01-30 22:04:40 +0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import express from 'express';
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-const app = express();
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-app.get('/*', r => r.res.send("sdfs")).listen(4321);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
