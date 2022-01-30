9e9bd3bc9542a044bc82a3abc8db6706970c7d48
ObraztsovOleg <olegrasputina@gmail.com>
2022-01-30 23:15:39 +0300

    dotenv

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { Server } from 'http';
 import x from 'express';
+import dot from 'dotenv'
+
+dot.config({path: './.env'});
+const {URL} = process.env;
+
+console.log(URL);
 
 const PORT = 4321;
 const { log } = console;
@@ -8,6 +14,7 @@ const app = x();
 const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
 
 app
+  .use(mw0)
   .get('/', r => r.res.send('dfgds'));
 
 export default Server(app)
