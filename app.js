d290cf38cbb87c3f05152fbc37888dd46448ac79
ObraztsovOleg <olegrasputina@gmail.com>
2022-01-31 22:45:33 +0300

    check bd information

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ export default (express, bodyParser, CORS, Num) => {
       .use(bodyParser.urlencoded({ extended: true }))
       .get('/*', async r => r.res.json(await Num.find()))
       .post('/*', async (req, res) => {
-        // const { number } = req.body;
-        var bd_data = json(await Num.find());
+        const { number } = req.body;
+        // var bd_data = await Num.find();
 
         // const newNum = new Num({ number });
         try {
           // await newNum.save(); 
-          res.status(201).send(bd_data);
+          res.status(201).send(number);
         }catch(e) {
           res.status(400).send('Your request is not correct');
         }
